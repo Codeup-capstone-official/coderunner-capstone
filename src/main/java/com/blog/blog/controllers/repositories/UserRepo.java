0665d7105e06f9ce832e38f1720577a366d9970a
christophermosqueda <chris.mosqueda8@gmail.com>
2018-09-17 14:46:58 -0500

    Updated UserController, repo, navbar

--- src/main/java/com/blog/blog/controllers/repositories/UserRepo.java
+++ src/main/java/com/blog/blog/controllers/repositories/UserRepo.java
@@ -1,11 +1,10 @@
-//package com.blog.blog.repositories;
-//
-//import com.blog.blog.models.User;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends CrudRepository<User, Long> {
-//    // Set up a method below
-//    User findByUsername(String username);
-//}
+package com.blog.blog.controllers.repositories;
+
+import org.apache.catalina.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
