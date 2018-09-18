1da294f008e9315eafde1f1dad29ae742d512b45
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-09-18 11:09:33 -0500

    final seeder

---
+++ seeder.sql
@@ -0,0 +1,16 @@
+insert into User(username, email, password, ranking) values (dewie, dewie@email.com, "password", "junior");
+insert into User(username, email, password, ranking) values (aaron, Aaron@email.com, "password", "senior");
+insert into User(username, email, password, ranking) values (Christian, Christian@email.com, "password", "senior");
+insert into User(username, email, password, ranking) values (Matt, Matt@email.com, "password", "wizard");
+insert into User(username, email, password, ranking) values (Zack, Zack@email.com, "password", "Mid");
+insert into User(username, email, password, ranking) values (Chris, Chris@email.com, "password", "Mid");
+insert into User(username, email, password, ranking) values (Voyageurs, Voy@email.com, "password", "junior");
+insert into User(username, email, password, ranking) values (Noob, Noob@email.com, "password", "Wizard");
+
+
+insert into Score(score, dateOfGame, user) values (100000, 01/01/2018, 1);
+insert into Score(score, dateOfGame, user) values (200000, 01/02/2018, 2);
+insert into Score(score, dateOfGame, user) values (300000, 01/03/2018, 3);
+insert into Score(score, dateOfGame, user) values (400000, 01/04/2018, 2);
+insert into Score(score, dateOfGame, user) values (500000, 02/01/2018, 4);
+insert into Score(score, dateOfGame, user) values (600000, 01/08/2018, 5);
