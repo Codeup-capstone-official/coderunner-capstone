b8ed97e0a1bce64d001b2bdbe04a9792143182bd
matthew rich <matt@matthews-MacBook-Pro.local>
2018-09-19 16:12:52 -0500

    seeder info

--- seeder.sql
+++ seeder.sql
@@ -12,8 +12,8 @@ insert into scores(score, date_of_game, user_id) values (100000, '01/01/2018', 1
 insert into scores(score, date_of_game, user_id) values (103325, '01/01/2018', 1);
 
 insert into scores(score, date_of_game, user_id) values (200000, '01/02/2018', 2);
-insert into scores(score, date_of_game, user_id) values (300000, '01/03/2018', 3);
+insert into scores(score, date_of_game, user_id) values (40000, '09/19/2018', 3);
 insert into scores(score, date_of_game, user_id) values (400000, '01/04/2018', 2);
 insert into scores(score, date_of_game, user_id) values (500000, '02/01/2018', 4);
-insert into scores(score, date_of_game, user_id) values (600000, '01/08/2018', 5);
+insert into scores(score, date_of_game, user_id) values (100000, '09/19/2018', 5);
 insert into scores(score, date_of_game, user_id) values (602330000, '09/18/2018', 5);
