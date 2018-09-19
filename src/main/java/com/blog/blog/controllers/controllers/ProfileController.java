9816b8ee58a27ea99046f5f33949d856d6632cc8
matthew rich <matt@matthews-MacBook-Pro.local>
2018-09-19 10:20:21 -0500

    added individual profile viewing

---
+++ src/main/java/com/blog/blog/controllers/controllers/ProfileController.java
@@ -0,0 +1,18 @@
+package com.blog.blog.controllers.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/{username}")
+    public String viewProfile(@PathVariable String username, Model model) {
+        model.addAttribute("username", username);
+        return "/profile";
+    }
+
+
+}
