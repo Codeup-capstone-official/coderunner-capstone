6e7108f13c37948ac9476fcfc3503255a4f75022
matthew rich <matt@matthews-MacBook-Pro.local>
2018-09-19 11:40:18 -0500

    added other profiles with multiple sources of data

--- src/main/java/com/blog/blog/controllers/controllers/ProfileController.java
+++ src/main/java/com/blog/blog/controllers/controllers/ProfileController.java
@@ -1,16 +1,39 @@
 package com.blog.blog.controllers.controllers;
 
+import com.blog.blog.controllers.models.User;
+import com.blog.blog.controllers.repositories.UserRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class ProfileController {
+    UserRepo userRepo;
+
+    public ProfileController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/profile")
+    public String viewOwnProfile() {
+       User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(user.getUsername());
+        return "/profile";
+
+    }
 
     @GetMapping("/profile/{username}")
     public String viewProfile(@PathVariable String username, Model model) {
-        model.addAttribute("username", username);
+        User user = userRepo.findByUsername(username);
+        String highscore = userRepo.highscore(username);
+        List<Object[]> last10Games = userRepo.latest10Games(username);
+        model.addAttribute("last10Games", last10Games);
+        model.addAttribute("highscore", highscore);
+        model.addAttribute("user", user);
         return "/profile";
     }
 
--- src/main/java/com/blog/blog/controllers/repositories/UserRepo.java
+++ src/main/java/com/blog/blog/controllers/repositories/UserRepo.java
@@ -1,6 +1,7 @@
 package com.blog.blog.controllers.repositories;
 
 
+import com.blog.blog.controllers.models.Score;
 import com.blog.blog.controllers.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -23,4 +24,11 @@ public interface UserRepo extends CrudRepository<User, Long> {
 
     @Query(value = "SELECT CURRENT_DATE();", nativeQuery = true)
     String currentDate();
+
+    @Query(value = "SELECT score FROM users JOIN scores s on users.id = s.user_id WHERE username = ?1 ORDER BY score DESC LIMIT 1;", nativeQuery = true)
+    String highscore(String username);
+
+    @Query(value = "SELECT date_of_game, score FROM users JOIN scores s on users.id = s.user_id WHERE username = ?1 ORDER BY date_of_game DESC LIMIT 10;", nativeQuery = true)
+    List<Object[]> latest10Games(String username);
+
 }
